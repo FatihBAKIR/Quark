d5485a940c5b1d49d01f1486f6e64ac069e263b7
Fatih BAKIR <fatih@linux.com>
2015-06-03 19:30:45 +0300

    TargetManager is completely removed from the project. Documentation updates

--- Source/Targeting/TargetMacro.cs
+++ Source/Targeting/TargetMacro.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Quark.Spells;
-using Quark.Utilities;
+﻿using Quark.Spells;
 using UnityEngine;
 
 namespace Quark.Targeting
@@ -21,24 +17,33 @@ namespace Quark.Targeting
         /// On targeting succession
         /// </summary>
         public event MacroSuccess TargetingSuccess = delegate { };
+
         /// <summary>
         /// On targeting failure
         /// </summary>
         public event MacroError TargetingFailed = delegate { };
+
         /// <summary>
         /// On a Targetable object selection
         /// </summary>
         public event TargetableDel TargetSelected = delegate { };
+
         /// <summary>
         /// On a Characater object selection
         /// </summary>
         public event CharacterDel CharacterSelected = delegate { };
+
         /// <summary>
         /// On a Point selection
         /// </summary>
         public event PointDel PointSelected = delegate { };
 
+        /// <summary>
+        /// The context this TargetMacro resides in.
+        /// </summary>
         protected Cast Context { get; private set; }
+
+#if DEBUG
         public TargetMacro()
         {
             Logger.GC(GetType().Name + "::ctor");
@@ -48,6 +53,7 @@ namespace Quark.Targeting
         {
             Logger.GC(GetType().Name + "::dtor");
         }
+#endif
 
         /// <summary>
         /// Begins the targeting procedure for this macro
@@ -62,11 +68,13 @@ namespace Quark.Targeting
         /// </summary>
         public virtual void Cancel()
         {
-            TargetManager.FreeTargeter();
             TargetingFailed(TargetingError.Cancelled);
             Clear();
         }
 
+        /// <summary>
+        /// Clear this TargetMacro from its context.
+        /// </summary>
         protected void Clear()
         {
             Context = null;
@@ -77,6 +85,11 @@ namespace Quark.Targeting
             PointSelected = null;
         }
 
+        /// <summary>
+        /// This method sets the context for this TargetMacro to run in.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="withoutCallbacks">If this flag is set, callbacks to the context will not be registered.</param>
         public virtual void SetContext(Cast context, bool withoutCallbacks = false)
         {
             Context = context;
@@ -89,6 +102,9 @@ namespace Quark.Targeting
             PointSelected += context.AddTarget;
         }
 
+        /// <summary>
+        /// This property stores the source Character for this targeting.
+        /// </summary>
         public Character Caster
         {
             get
@@ -100,31 +116,46 @@ namespace Quark.Targeting
         /// <summary>
         /// On Target Selection
         /// </summary>
-        /// <param name="target">Selected target</param>
+        /// <param name="target">Selected Character</param>
         protected void OnTargetSelected(Character target)
         {
             _targets.Add(target);
             CharacterSelected(target);
         }
 
+        /// <summary>
+        /// On Target Selection
+        /// </summary>
+        /// <param name="target">Selected Point</param>>
         protected void OnTargetSelected(Vector3 target)
         {
             _targets.Add(target);
             PointSelected(target);
         }
 
+        /// <summary>
+        /// On Target Selection
+        /// </summary>
+        /// <param name="target">Selected Targetable</param>
         protected void OnTargetSelected(Targetable target)
         {
             _targets.Add(target);
             TargetSelected(target);
         }
 
+        /// <summary>
+        /// On Targeting Success
+        /// </summary>
         protected void OnTargetingSuccess()
         {
             TargetingSuccess(_targets);
             _targets = new TargetCollection();
         }
 
+        /// <summary>
+        /// On Targeting Fail
+        /// </summary>
+        /// <param name="error">The reason for this targeting to fail.</param>
         protected void OnTargetingFail(TargetingError error)
         {
             TargetingFailed(error);
@@ -134,6 +165,9 @@ namespace Quark.Targeting
         TargetCollection _targets = new TargetCollection();
     }
 
+    /// <summary>
+    /// This enumeration represents the reason a targeting has failed.
+    /// </summary>
     public enum TargetingError
     {
         /// <summary>
@@ -148,5 +182,46 @@ namespace Quark.Targeting
         /// No viable target was found
         /// </summary>
         NotFound
+    }
+
+    /// <summary>
+    /// This enumeration represents a Spells target form.
+    /// </summary>
+    public enum TargetForm
+    {
+        /// <summary>
+        /// May hit multiple characters
+        /// </summary>
+        Singular,
+        /// <summary>
+        /// May not hit multiple characters
+        /// </summary>
+        Plural
+    }
+
+    /// <summary>
+    /// This enumeration represents a Targets type.
+    /// </summary>
+    public enum TargetType
+    {
+        /// <summary>
+        /// There is no target.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Target is a Point.
+        /// </summary>
+        Point,
+
+        /// <summary>
+        /// The target is a Targetable.
+        /// </summary>
+        Targetable,
+
+        /// <summary>
+        /// The target is a Character
+        /// </summary>
+        Character
     }
 }
